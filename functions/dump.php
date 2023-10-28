7e0145404ceadb73c559ed6280d742f23b77479b
Arnaud BODEL <arnaud.bodel@gmail.com>
2023-10-28 22:17:35 +0200

    -

--- functions/dump.php
+++ functions/dump.php
@@ -5,10 +5,10 @@ function dump(mixed $data, bool $varDump = false): void
     $traces = debug_backtrace();
     $caller = $traces[0];
     
-    echo '<div class="dump">';
-    echo '<pre class="dump-data">';
+    echo '<div style="background-color: #2c3645; border-radius: 6px; margin-bottom: 30px;">';
+    echo '<pre style="border-radius: 6px; padding: 15px; background-color: #050d18; color: #FFFFFF; margin: 0;">';
     $varDump ? var_dump($data) : print_r($data);
     echo '</pre>';
-    echo '<div class="dump-caption">'.$caller['file'].":".$caller['line'].'</div>';
+    echo '<div style="font-weight: 700; font-size: .8rem; color: #ffffff; padding: 8px 15px;">'.$caller['file'].":".$caller['line'].'</div>';
     echo '</div>';
 }
