223e58b79971733ade375c57f0663d0f9b179bdd
Arnaud BODEL <arnaud.bodel@gmail.com>
2023-10-28 21:50:32 +0200

    -

--- functions/dump.php
+++ functions/dump.php
@@ -5,8 +5,8 @@ function dump(mixed $data, bool $varDump = false): void
     $traces = debug_backtrace();
     $caller = $traces[0];
     
-    echo "<div>".$caller['file'].":".$caller['line']."</div>";
     echo '<pre class="dump">';
     $varDump ? var_dump($data) : print_r($data);
     echo '</pre>';
+    echo '<div class="dump-caption">'.$caller['file'].":".$caller['line'].'</div>';
 }
